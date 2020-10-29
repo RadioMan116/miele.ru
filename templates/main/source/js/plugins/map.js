a68a89e8f1cdd357cb9eac960034597571fd8998
RadioMan116 <noris75564@gmail.com>
2020-10-29 13:09:31 +0300

    #контент Сделать верстку страницы о бренде miele.ret-team.ru

--- templates/main/source/js/plugins/map.js
+++ templates/main/source/js/plugins/map.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	let checkMap = document.querySelector(".maps");
 	if (checkMap) {
 		var myPlacemark;
@@ -8,16 +8,16 @@ $(document).ready(function() {
 			if (!element) {
 				return false;
 			}
-			ymaps.ready(function() {
+			ymaps.ready(function () {
 				myPlacemark;
 				myMap = new ymaps.Map(element, {
 					center: [parseFloat(center[0]), parseFloat(center[1])],
 					zoom: zoom
 				}, {
-					searchControlProvider: 'yandex#search'
+					searchControlProvider: "yandex#search"
 				});
-				myMap.controls.remove('searchControl');
-				myMap.behaviors.disable('scrollZoom');
+				myMap.controls.remove("searchControl");
+				myMap.behaviors.disable("scrollZoom");
 				for (var i = 0, l = coords.length; i < l; i += 1) {
 					myPlacemark = new ymaps.Placemark([parseFloat(coords[i].x), parseFloat(coords[i].y)], {
 						// iconCaption: coords[i].c,
@@ -34,23 +34,23 @@ $(document).ready(function() {
 						</div>
 						`,
 					}, {
-						preset: 'islands#redIcon',
+						preset: "islands#redIcon",
 					});
 					myMap.geoObjects.add(myPlacemark);
 				}
 			});
 		}
 		// contact map
-		(function() {
-			var items = document.querySelectorAll('.js-warranty__map-point');
-			var map = document.getElementById('js-warranty__map');
-			var center = (map.getAttribute('data-center')).split(',');
+		(function () {
+			var items = document.querySelectorAll(".js-warranty__map-point");
+			var map = document.getElementById("js-warranty__map");
+			var center = (map.getAttribute("data-center")).split(",");
 			if (!map) {
 				return false;
 			}
-			var coords = []
-			$(items).each(function(index) {
-				var tmp = (this.getAttribute('data-coords')).split('||')
+			var coords = [];
+			$(items).each(function (index) {
+				var tmp = (this.getAttribute("data-coords")).split("||");
 				coords[index] = {
 					x: tmp[0],
 					y: tmp[1],
@@ -58,9 +58,9 @@ $(document).ready(function() {
 					t: tmp[3],
 					a: tmp[4],
 					b: tmp[5]
-				}
+				};
 			});
-			console.log(coords)
+			console.log(center);
 			createMaps(map, coords, center, 9);
 			// createMaps(map, coords, tmp, 14);
 		})();
